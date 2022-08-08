1667e3614761cb99663d3334a4a23f178d20a47e
popo26 <mynameispopo26@gmail.com>
2022-08-09 07:55:39 +1200

    API with Javascript

---
+++ WeatherProject/app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");//necessary for parsing body of post request
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));//necessary for parsing body of post request
+
+app.get("/", function(req, res){
+ res.sendFile(__dirname + "/index.html");
+
+
+  //     const object = {
+  //       name:"Ai",
+  //       favouriteFood:"Wine",
+  //     }
+  //     console.log(JSON.stringify(object));
+  //   })
+  // })
+
+});
+
+app.post("/", function(req, res){
+
+  const query = req.body.cityName;
+  const apiKey = "myAPI";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+    response.on("data", function(data){
+      //console.log(data); //hexadecimal
+      const weatherData=JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const weatherIcon = weatherData.weather[0].icon;
+      const imageURL = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+      console.log(weatherDescription);
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write("<img src=" + imageURL + ">");
+      res.write("<h1>The temperature in " + query + " is " + temp + "degrees Celcius.</h1>");
+      res.send();
+
+    });
+  });
+
+
+})
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
+})
